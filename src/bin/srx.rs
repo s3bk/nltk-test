fb6d214d98a5ff468dceea879b80ad18638697f1
Sebastian K <s3bk@protonmail.com>
2021-03-03 18:25:16 +0300

    remove srx

--- src/bin/srx.rs
+++
@@ -1,8 +0,0 @@
-use nltk_test::{run};
-use srx::{SRX, Rules};
-
-fn main() {
-    let srx: SRX = include_str!("../../data/segment.srx").parse().unwrap();
-    let english_rules = srx.language_rules("en").compile();
-    //run(|text| english_rules.split(text)
-}
